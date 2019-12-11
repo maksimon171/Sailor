4f35c53bf5dca96bb6a38f69801245d62218ff77
Maksim Antonov <maksim_antonov17@mail.ru>
2019-12-11 19:53:13 +0300

    Sailor

---
+++ source/repos/Program3/Program3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Program3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number_n = 0;
+            int number_f = 0;
+
+            for (int number_k = -41; number_k >= -121; number_k--)
+            {
+                number_n = number_n + number_k;
+                number_f++;
+            }
+
+            Console.WriteLine("Ответ = {0}",
+                number_n / number_f); ;
+            Console.ReadKey();
+        
+        }
+    }
+}
